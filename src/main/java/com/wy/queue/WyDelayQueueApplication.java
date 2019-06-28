6cb01f51ae0879a80f9677ba961b7d0928ff8b08
肖家豪 <qq584686739@gmail.com>
2019-06-28 15:35:59 +0800

    add WyDelayQueueApplication

--- src/main/java/com/wy/queue/WyDelayQueueApplication.java
+++ src/main/java/com/wy/queue/WyDelayQueueApplication.java
@@ -1,8 +1,14 @@
-package com.wy.queue.wydelayqueue;
+package com.wy.queue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 描述:启动类
+ *
+ * @author xjh
+ * created on 2019-06-28 15:34
+ */
 @SpringBootApplication
 public class WyDelayQueueApplication {
 
