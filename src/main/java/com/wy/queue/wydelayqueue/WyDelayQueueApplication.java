c8e8337eb4edc491f7ea49521ebb899e0742f3b6
肖家豪 <qq584686739@gmail.com>
2019-06-28 15:33:45 +0800

    Initial commit

---
+++ src/main/java/com/wy/queue/wydelayqueue/WyDelayQueueApplication.java
@@ -0,0 +1,13 @@
+package com.wy.queue.wydelayqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WyDelayQueueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WyDelayQueueApplication.class, args);
+    }
+
+}
